4d49791c82bce4178fb69d9967ea012b38106b5f
QuietOne <tihomir.radosavljevic@gmail.com>
2014-06-26 22:32:50 +0200

    something done

--- src/java/domain/AnsweredPK.java
+++ src/java/domain/AnsweredPK.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package domain;
 
 import java.io.Serializable;
@@ -14,7 +8,8 @@ import javax.validation.constraints.NotNull;
 
 /**
  *
- * @author tihomir
+ * @author Tihomir Radosavljvic
+ * @version 1.0
  */
 @Embeddable
 public class AnsweredPK implements Serializable {
--- src/java/domain/Testitem.java
+++ src/java/domain/Testitem.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package domain;
 
 import java.io.Serializable;
@@ -23,7 +17,8 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author tihomir
+ * @author Tihomir Radosavljvic
+ * @version 1.0
  */
 @Entity
 @Table(name = "testitem")

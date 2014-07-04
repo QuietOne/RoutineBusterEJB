b88551c8956cc15fea2a0f3132baba40d2183881
QuietOne <tihomir.radosavljevic@gmail.com>
2014-07-04 10:43:33 +0200

    gamechooser

--- src/java/domain/Category.java
+++ src/java/domain/Category.java
@@ -147,7 +147,7 @@ public class Category implements Serializable {
 
     @Override
     public String toString() {
-        return "domain.Category[ idCat=" + idCat + " ]";
+        return name;
     }
     
 }
